d18e3d09d49518db0f39ba8f68ce0bed2603ff2c
Suraj Yadav <sy429843@gmail.com>
2025-09-30 17:40:32 +0530

    changing the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import Contact from './components/contact/Contact';
-import Experience from './components/experience/Experience';
-import Footer from './components/footer/Footer';
 
-import Portfolio from './components/portfolio/Portfolio';
-import Testimonials from './components/testimonials/Testimonials';
-import Topbar from './components/topbar/Topbar';
-import Header from './components/header/Header';
-import Intro from './components/intro/Intro';
-
+import React from "react";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Topbar from "./components/topbar/Topbar";
+import Header from "./components/header/header";  // ✅ Fixed case
+import Intro from "./components/intro/intro";      // ✅ Make sure file is Intro.jsx
 
 const App = () => {
   return (
@@ -22,7 +21,7 @@ const App = () => {
       <Contact />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
